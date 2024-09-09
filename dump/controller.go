0396c2847436765a85f5f1a1f17a3e8912c9968f
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-09-09 09:48:47 +0200

    Mise en place des routes pour les backups

---
+++ dump/controller.go
@@ -0,0 +1,78 @@
+package dump
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+//DONE 1. Create a route create a backup
+//DONE 2. Create a route to get all backups
+//TODO 3. Create a route to get a backup by id
+//TODO 4. Create a route to delete a backup by id
+
+func AddRoutes(app *fiber.App) {
+
+	app.Post( "/backup", func(ctx *fiber.Ctx) error {
+		backup := new(DumpModel)
+		if err := ctx.BodyParser(backup); err != nil {
+		return ctx.Status(200).JSON(fiber.Map{
+			"success": false,
+			"message": err.Error(),
+			})
+		} else {
+			return ctx.Status(200).JSON(fiber.Map{
+				"success": true,
+				"message": "Backup created successfully",
+			})
+		}
+	})
+
+
+		app.Get("/backups", func(ctx *fiber.Ctx) error {
+			backup := new(DumpModel)
+			if err := ctx.BodyParser(backup); err != nil {
+			return ctx.Status(200).JSON(fiber.Map{
+				"success": true,
+				"message": "All backups",
+			})
+		} else {
+			return ctx.Status(200).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+	})
+
+
+	app.Get("/backup/:id", func(ctx *fiber.Ctx) error {
+		backup := new(DumpModel)
+		if err := ctx.BodyParser(backup); err != nil {
+		return ctx.Status(200).JSON(fiber.Map{
+			"success": true,
+			"message": "Backup by id",
+			})
+		} else {
+			return ctx.Status(200).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+	})
+
+
+	app.Delete("/backup/:id", func(ctx *fiber.Ctx) error {
+		backup := new(DumpModel)
+		if err := ctx.BodyParser(backup); err != nil {
+		return ctx.Status(200).JSON(fiber.Map{
+			"success": true,
+			"message": "Backup deleted",
+			})
+		} else {
+			return ctx.Status(200).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+			})
+		}
+	})
+
+}
+
--- dump/model.go
+++ dump/model.go
@@ -2,10 +2,11 @@ package dump
 
 import "cloud.google.com/go/civil"
 
+
 type DumpModel struct {
 	Id	   int
 	Name	 string
-	Cron_job   string
+	Cron_job  *string
 	Connection_id int
 	Created_at civil.DateTime
 }
