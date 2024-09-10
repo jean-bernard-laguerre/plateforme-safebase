35c719c9920c89a14c2c8b4f52fef4da7c5b35d0
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2024-09-10 11:02:53 +0200

    connection service test

---
+++ connection/test/repository_test.go
@@ -0,0 +1,29 @@
+package connection_test
+
+import (
+	"testing"
+)
+
+func TestRepository(t *testing.T) {
+	/* t.Run("GetById", func(t *testing.T) {
+		conn := connection.ConnectionModel{}
+		result, err := conn.GetById(1)
+		if err != nil {
+			t.Fatalf("GetById failed: %v", err)
+		}
+		if result.Id != 1 {
+			t.Fatalf("GetById failed")
+		}
+	})
+
+	t.Run("GetByUserId", func(t *testing.T) {
+		conn := connection.ConnectionModel{}
+		result, err := conn.GetByUserId(1)
+		if err != nil {
+			t.Fatalf("GetByUserId failed: %v", err)
+		}
+		if len(result) == 0 {
+			t.Fatalf("GetByUserId failed")
+		}
+	}) */
+}
