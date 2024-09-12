7f76e53e1810dc2c30b53d2ed31466b54d85697f
thomas-spinec <thomas.spinec@laplateforme.io>
2024-09-12 16:14:25 +0200

    add interface to type user

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,13 +1,29 @@
 import instance from "./config";
 
+interface User {
+  email: "string";
+  password: "string";
+}
+
 export const actions = {
   //REGISTER
-  async register(user: JSON) {
+  async register(user: User) {
     try {
       const response = await instance.post("/register", JSON.stringify(user));
       return response.data;
     } catch (err) {
-      console.log(err);
+      console.log("erreur lors de l'inscription:", err);
+      return err;
+    }
+  },
+
+  //LOGIN
+  async login(user: User) {
+    try {
+      const response = await instance.post("/login", JSON.stringify(user));
+      return response.data;
+    } catch (err) {
+      console.log("erreur lors de la connection:", err);
       return err;
     }
   },
