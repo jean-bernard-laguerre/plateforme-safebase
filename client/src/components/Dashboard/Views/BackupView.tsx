4cd08938f9b87d626100a9856c6830adda0914ee
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-09-19 10:39:24 +0200

    Refactor BackupView component to add tooltip for adding a new backup

--- client/src/components/Dashboard/Views/BackupView.tsx
+++ client/src/components/Dashboard/Views/BackupView.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import { Tooltip } from "@nextui-org/tooltip";
+import { DatabaseZap } from "lucide-react";
+import { useState } from "react";
 
 const BackupView = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <p>Backup View</p>
+    <div className="p-4 col-span-12 bg-transparent rounded border border-stone-300 mx-2">
+      <div className="mb-4 flex items-center justify-between">
+        <h3 className="flex items-center gap-1.5 font-medium">
+          <DatabaseZap />
+          BackUps
+        </h3>
+        <Tooltip
+          content="Add a new backup"
+          placement="left"
+          showArrow
+          className="border border-slate-300 text-sm bg-slate-500 text-stone-50 rounded p-1 shadow-sm"
+        >
+          <button
+            onClick={handleOpenModal}
+            className="text-sm font-semibold bg-violet-400 text-stone-50 p-1 rounded border  border-violet-500"
+          >
+            Add +
+          </button>
+        </Tooltip>
+      </div>
     </div>
   );
 };
