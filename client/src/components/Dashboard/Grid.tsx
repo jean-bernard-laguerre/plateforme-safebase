e4242df3d2892f66bb5c46f7b36c496dfef40075
mohamed-necib <mohamed-el-amine.necib@laplateforme.io>
2024-09-22 22:52:13 +0200

    Add Tutorial component to Dashboard Grid

--- client/src/components/Dashboard/Grid.tsx
+++ client/src/components/Dashboard/Grid.tsx
@@ -1,9 +1,13 @@
 import { StatCards } from "./StatCard";
+import Tutorial from "./Tutorial";
 
 export const Grid = () => {
   return (
     <div className="px-4 grid gap-3 grid-cols-12">
       <StatCards />
+      <div className="col-span-12 mt-8">
+        <Tutorial />
+      </div>
     </div>
   );
 };
